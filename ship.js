9c355ab89fb8f54da5fbbd6c8f466606c3c2161c
Marc <147423007+marcaixs@users.noreply.github.com>
2025-07-18 15:55:17 +0200

    create ship class

---
+++ ship.js
@@ -0,0 +1,13 @@
+export class Ship {
+    constructor(length){
+        this.length = length;
+        this.hits = 0;
+        this.sunk = false;
+    }
+    hit(){
+        this.hits ++;
+    }
+    isSunk(){
+        if (this.length <= this.hits) this.sunk = true;
+    }
+}
