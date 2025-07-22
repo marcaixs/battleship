4e94cae3ac6d38df7544b0d3dd54d11e388de43c
Marc <147423007+marcaixs@users.noreply.github.com>
2025-07-22 12:46:55 +0200

    gameplay first draft

---
+++ gameplay.js
@@ -0,0 +1,7 @@
+import { Ship } from "./ship.js";
+import { Gameboard } from "./gameboard.js";
+import { Player } from "./player.js";
+
+const player = new Player('human');
+const computerPlayer = new Player('computer');
+
