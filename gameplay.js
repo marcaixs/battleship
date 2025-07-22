83d9364722e2ec8e1dc425e33bd1bef9e180300a
Marc <147423007+marcaixs@users.noreply.github.com>
2025-07-22 17:25:58 +0200

    correct gamplay

--- gameplay.js
+++ gameplay.js
@@ -1,14 +1,20 @@
-import { Ship } from "./ship.js";
-import { Gameboard } from "./gameboard.js";
 import { Player } from "./player.js";
 
+// 0 - Initialize players
 const player = new Player('Player');
 const computerPlayer = new Player('Computer');
 
+// 1 - Initialize boards
 const board1 = document.getElementById('gameboard1');
 const board2 = document.getElementById('gameboard2');
 
-player.gameboard.renderGameboard(player.type, board1);
-computerPlayer.gameboard.renderGameboard(computerPlayer.type, board2);
+// 2 - Render boards
+player.gameboard.renderGameboard(board1);
+computerPlayer.gameboard.renderGameboard(board2);
 
 
+// 3 - Enable click events to cells
+
+// 4 - Place ships
+
+// 5 - Handle turns
