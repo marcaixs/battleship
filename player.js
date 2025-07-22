731f113f0b39ba4f1ff9e601a2fa0b8154f38720
Marc <147423007+marcaixs@users.noreply.github.com>
2025-07-22 14:11:49 +0200

    add createGrid call in constructor

--- player.js
+++ player.js
@@ -3,6 +3,7 @@ import { Gameboard } from "./gameboard.js"
 export class Player{
     constructor(type){
         this.gameboard = new Gameboard();
+        this.gameboard.createGrid();
         this.type = type;
     }
 }
