c77be54686eb5cd6d57d8135984840d5967a0882
Marc <147423007+marcaixs@users.noreply.github.com>
2025-07-22 12:46:45 +0200

    gameboard basic implementation

--- gameboard.js
+++ gameboard.js
@@ -1,25 +1,58 @@
+import { Ship } from './ship.js'
+
 export class Gameboard {
     constructor(){
         this.grid = [];
         createGrid();
-        this.ships = 5;
+
+        const ship1 = new Ship(5,'vertical', []);
+        const ship2 = new Ship(5,'vertical', []);
+        const ship3 = new Ship(5,'vertical', []);
+
+        this.ships = [ship1, ship2, ship3];
     }
 
     createGrid(){
         for(let i = 0; i<10; i++){
-             for(let j = 0; j<100; j++){
+             for(let j = 0; j<10; j++){
                     const cell = new Cell(i, j);
                     this.grid.push(cell)
                 }
         }
     }
 
-    placeShip(ship){
+    placeShip(ship, x, y){
+        ship.startingCell.push(x, y)
 
+        if(ship.direction == 'vertical'){
+            for (let i = 0; i<ship.length; i++){
+                ship.cells.push(this.grid[x][y+i])
+            }
+        }
+
+        if(ship.direction == 'horizontal'){
+            for (let i = 0; i<ship.length; i++){
+                ship.cells.push(this.grid[x+i][y])
+            }
+        }
+        
     }
 
-    recieveAttack(){
-        
+    recieveAttack(x,y){
+        if(this.grid[x,y].played == false){
+            this.ships.forEach((ship)=>{
+             ship.find((coordenates)=>{
+                if(coordenates.x == x && coordenates.y == y){
+                    ship.hit();
+                    if(ship.isSunk()==true){
+                        this.ships - 1;
+                    }
+                }
+             })   
+        })
+
+        this.grid[x,y].played = true;
+        }     
     }
 }
 
@@ -27,9 +60,6 @@ class Cell {
     constructor(x, y){
         this.x = x;
         this.y = y;
-        this.position = String(x) + String(y);
-        this.ship = false;
-        this.missed = false;
-        this.destroyed = false;
+        this.played = false;
     }
 }
