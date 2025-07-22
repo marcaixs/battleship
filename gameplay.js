5c7c2bfc4ed6cc9170d21c27bd967782d4f01c8a
Marc <147423007+marcaixs@users.noreply.github.com>
2025-07-22 14:10:52 +0200

    render boards

--- gameplay.js
+++ gameplay.js
@@ -2,6 +2,24 @@ import { Ship } from "./ship.js";
 import { Gameboard } from "./gameboard.js";
 import { Player } from "./player.js";
 
-const player = new Player('human');
-const computerPlayer = new Player('computer');
+const player = new Player('Player');
+const computerPlayer = new Player('Computer');
+
+const board1 = document.getElementById('gameboard1');
+const board2 = document.getElementById('gameboard2');
+
+function fillGameboard(type, board){
+    for(let i = 0; i<10; i++){
+        for(let j=0; j<10; j++){
+            const cell = document.createElement('div');
+            cell.classList.add('cell'+type);
+            cell.classList.add(i+''+j);
+            board.appendChild(cell);
+        }
+    }    
+}
+
+fillGameboard(player.type, board1);
+fillGameboard(computerPlayer.type, board2);
+
 
