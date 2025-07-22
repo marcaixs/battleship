3d97dfc38c1cc7887f11c9f093fdefb0f3d5cd53
Marc <147423007+marcaixs@users.noreply.github.com>
2025-07-22 14:16:24 +0200

    move render gameboard to gameboard class

--- gameboard.js
+++ gameboard.js
@@ -52,6 +52,17 @@ export class Gameboard {
         this.grid[x,y].played = true;
         }     
     }
+
+    renderGameboard(type, board){
+         for(let i = 0; i<10; i++){
+            for(let j=0; j<10; j++){
+                const cell = document.createElement('div');
+                cell.classList.add('cell'+type);
+                cell.classList.add(i+''+j);
+                board.appendChild(cell);
+            }
+        }    
+    }
 }
 
 class Cell {
--- gameplay.js
+++ gameplay.js
@@ -8,18 +8,7 @@ const computerPlayer = new Player('Computer');
 const board1 = document.getElementById('gameboard1');
 const board2 = document.getElementById('gameboard2');
 
-function fillGameboard(type, board){
-    for(let i = 0; i<10; i++){
-        for(let j=0; j<10; j++){
-            const cell = document.createElement('div');
-            cell.classList.add('cell'+type);
-            cell.classList.add(i+''+j);
-            board.appendChild(cell);
-        }
-    }    
-}
-
-fillGameboard(player.type, board1);
-fillGameboard(computerPlayer.type, board2);
+player.gameboard.renderGameboard(player.type, board1);
+computerPlayer.gameboard.renderGameboard(computerPlayer.type, board2);
 
 
